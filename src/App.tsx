51646557ba141deef5a617d4d123feb29ac14d6c
Cristian Navarro <canavarroq@ufpso.edu.co>
2026-03-11 17:25:24 -0500

    fix: corrección de ubicación precisa y estabilidad del marcador en el mapa

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/mapa" element={<Index />} />
